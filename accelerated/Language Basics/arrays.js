68edb7b59f5f3d912ca34987848bb8f47ff46f60
austinwilson1224 <austin1224@gmail.com>
2020-08-05 15:02:02 -0700

    slice

--- accelerated/Language Basics/arrays.js
+++ accelerated/Language Basics/arrays.js
@@ -11,3 +11,36 @@ array = [1,2,3];
 array.forEach(function(element) {
     console.log(element);
 });
+array.push(4);
+console.log(array);
+array = [1,2,3,];
+console.log(array);
+array.push(4);
+console.log(array);
+array = [1,2,3,,];
+console.log(array);
+array.push(4);
+console.log(array);
+console.log(array.pop());
+array.pop();
+console.log(array);
+array.shift();
+console.log(array);
+array.unshift("new element");
+console.log(array);
+
+
+// splice and slice 
+console.log(array.indexOf(2));
+array[array.indexOf("new element")] = "old";
+console.log(array);
+
+var newArray = array.splice(0,1);
+
+console.log(newArray);
+console.log(array);
+
+var newArray2 = newArray.slice(0);
+console.log(newArray2);
+
+
