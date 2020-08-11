5dc0dbd25543e8cd0f39d7ad804996b87e857eff
austinwilson1224 <austin1224@gmail.com>
2020-08-11 16:28:10 -0700

    js stuff

--- accelerated/built_in_obj_methods/script.js
+++ accelerated/built_in_obj_methods/script.js
@@ -10,3 +10,27 @@ var interval = setInterval(function() {
 setTimeout(function() {
     clearInterval(interval);
 }, 3000);
+
+var a = '5';
+var b = 'FBB123';
+var c = 10;
+var d = 10.3;
+console.log(a);
+console.log(parseInt(b,16));
+console.log(c.toString());
+console.log(d.toFixed());
+console.log(d.toFixed(3));
+
+
+// string functions 
+var string = 'Any text';
+console.log(string);
+console.log(string.length);
+console.log(string[2]);
+console.log(string.charAt(2));
+var string2 = string.concat(', and some more text!');
+console.log(string2.toUpperCase());
+console.log(string.split(' '));
+
+var string3 = '        Any     text    ';
+console.log(string3.trim());
