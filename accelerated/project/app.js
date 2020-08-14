0d428f7426d6c97691e7caed597663c40a9f0088
austinwilson1224 <austin1224@gmail.com>
2020-08-14 14:43:57 -0700

    http request setup and weather object creation

--- accelerated/project/app.js
+++ accelerated/project/app.js
@@ -12,4 +12,13 @@ function searchWeather() {
     var http = new XMLHttpRequest();
     var apiKey = '2a4cd1aa0d21a9d50fe3358ff31d3f87';
     var url = 'api.openweathermap.org/data/2.5/weather?q=' + cityName + apiKey;
+    var method = 'GET';
+    http.open(method,url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+        } else if (http.readyState == XMLHttpRequest.DONE) {
+            console.log("something went wrong");
+        }
+    }
 }
