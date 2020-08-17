0ddf6d42dd3bd1983c5f1f370ac7d8fa1a9da1f1
austinwilson1224 <austin1224@gmail.com>
2020-08-17 08:28:32 -0700

    started es6 review

---
+++ es6/syntax/script.js
@@ -0,0 +1,22 @@
+var name = 'Austin';
+console.log(name);
+
+
+// let has block scope
+let name1 = 'Austin';
+console.log(name1);
+
+if (true) {
+    var name3 = 'aa';
+    let name4 = 'bb';
+}
+console.log(name3);
+// will throw error
+// console.log(name4);
+
+let age = 30;
+if (true) {
+    let age = 27;
+    console.log(age);
+}
+console.log(age);
