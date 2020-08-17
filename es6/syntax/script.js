dcb78172e0e848d6a830d37e2280ce5039778598
austinwilson1224 <austin1224@gmail.com>
2020-08-17 08:32:31 -0700

    const review

--- es6/syntax/script.js
+++ es6/syntax/script.js
@@ -20,3 +20,22 @@ if (true) {
     console.log(age);
 }
 console.log(age);
+
+// const 
+const AGE = 27;
+// AGE = 27; won't work
+console.log(AGE);
+const AGES = [27, 29, 31];
+console.log(AGES);
+AGES.push(25);
+console.log(AGES);
+
+
+const OBJ = {
+    age: 27
+};
+
+console.log(OBJ);
+
+OBJ.age = 30;
+console.log(OBJ);
