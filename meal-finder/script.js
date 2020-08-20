829d0655d8a521846750aaa7dcc5cb33839d37a9
austinwilson1224 <austin1224@gmail.com>
2020-08-20 09:44:21 -0700

    js

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -27,14 +27,15 @@ function searchMeal(e) {
                 console.log(data);
                 resultHeading.innerHTML = `<h2>Search results for ${term}:</h2`;
 
-                if(data.mealsEl === null) {
+                if(data.meals === null) {
                     resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
                 } else {
                     mealsEl.innerHTML = data.meals.map(meal => `
-                    <div class="meal">
-                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                        <div class="meal-info" data-mealID="${meal.idMeal}">
-                            <h3>${meal.strMeal}</h3>
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
                         </div>
                     `)
                     .join('');
