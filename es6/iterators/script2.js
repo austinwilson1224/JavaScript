095cc09b47c0c811dbb8dddc4097baf9de755841
austinwilson1224 <austin1224@gmail.com>
2020-08-21 16:04:19 -0700

    generators

--- es6/iterators/script2.js
+++ es6/iterators/script2.js
@@ -1,1 +1,32 @@
 // generators
+function *select() {
+    yield 'House';
+    yield 'Garage';
+}
+
+let it = select();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+let obj = {
+    [Symbol.iterator]: gen
+}
+
+function *gen(end) {
+    for (let i = 0; i < end; i++) {
+        yield i;
+    }
+    // yield 1;
+    // yield 2;
+}
+
+let it2 = gen(10);
+console.log(it2.next());
+console.log(it2.throw());
+console.log(it2.next());
+console.log(it2.next());
+console.log(it2.next());
+// for (let element of obj) {
+//     console.log(element);
+// }
