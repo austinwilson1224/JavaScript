b18917c8d0e12b3dd04c4925e84713bc6a5836c1
austinwilson1224 <austin1224@gmail.com>
2020-08-20 19:55:37 -0700

    more es6

--- es6/syntax/script.js
+++ es6/syntax/script.js
@@ -1,3 +1,5 @@
+
+
 var name = 'Austin';
 console.log(name);
 
@@ -70,3 +72,92 @@ console.log(add(3,2));
 
 var subtract = a => a - 2;
 console.log(subtract(2));
+var button = document.querySelector('button');
+button.addEventListener('click', fn22);
+
+
+function fn() {
+    console.log(this);
+}
+
+fn();
+
+var fn22 = () => console.log(this);
+
+fn2();
+
+// functions and default parameters 
+function isEqualTo(number = 0, compare = 0) {
+    return number == compare;
+}
+
+console.log(isEqualTo(10,10));
+console.log(isEqualTo(10)); // default value
+console.log(isEqualTo());
+
+// object literal extensions 
+let namee = 'Austin';
+let agee = 27;
+let ageField = "age";
+let obj = {
+    namee: 'Austin2',
+    agee,
+    [ageField]: 28,
+    greet() {
+        console.log(this.namee +','+ this.agee);
+    },
+    "greet me"() {
+        console.log('ello puppet');
+    }
+}
+console.log(obj);
+obj.greet();
+obj["greet me"];
+
+// rest operator
+
+let array = [1,2,3,4,5];
+function sumup(...args) {
+    let result = 0;
+    for (let i = 0; i < args.length; i++) {
+        result += args[i]
+    }
+    return result;
+}
+let result = sumup(111,1,112,1);
+console.log(result);
+
+// spread operator
+let number2 = [1,2,3,4,5];
+console.log(Math.max(number2)); // doesn't work
+
+
+console.log(...number2);
+console.log(Math.max(...number2))
+
+// for of loop
+let testResults = [1.2,3.4,22.4,124.2];
+for(let testResult of testResults) {
+    console.log(testResult);
+}
+
+
+// template literals 
+let description = `
+Like this! Hello I'm ${name + "!!!"}
+\${name} this backslash will escape the template literal
+`;
+
+console.log(description);
+
+// destructuring arrays 
+let numbars = [1,2,3];
+let [a, b] = numbars;
+let [c, ... d] = numbars;
+console.log(d)
+console.log(a,b);
+
+let e = 4;
+let ff = 7;
+[ff, e] = [e, ff];
+console.log(ff,e);
