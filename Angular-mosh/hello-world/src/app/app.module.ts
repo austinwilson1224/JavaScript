194d2b03a0b39325341c9327e5667bbdd7760690
austinwilson1224 <austin1224@gmail.com>
2021-03-09 08:16:39 -0800

    angular

--- Angular-mosh/hello-world/src/app/app.module.ts
+++ Angular-mosh/hello-world/src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { AuthorsService } from './authors.service';
+import { EmailService } from './email.service';
+import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    EmailService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
