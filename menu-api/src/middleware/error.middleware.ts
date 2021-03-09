4d35c568d6c537489e86451caf783bfc5fe8a4fe
austinwilson1224 <austin1224@gmail.com>
2021-03-09 08:17:02 -0800

    Idk what this is

---
+++ menu-api/src/middleware/error.middleware.ts
@@ -0,0 +1,13 @@
+// src/middleware/error.middleware.ts
+
+import HttpException from "../common/http-exception";
+import { Request, Response, NextFunction, request } from "express";
+
+export const errorHandler = (
+    error: HttpException,
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    const status = error.statusCode || error.status || 500;
+}
