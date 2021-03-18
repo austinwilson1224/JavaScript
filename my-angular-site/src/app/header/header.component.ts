a869486f60b5b9f27685bb831623b94a47c18114
austinwilson1224 <austin1224@gmail.com>
2021-03-18 16:40:27 -0700

    angular homepage :

---
+++ my-angular-site/src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <div class="navbar is-dark">
+    <!-- logo -->
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        MY LOGO GOES HERE
+      </a>
+    </div>
+  </div>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
