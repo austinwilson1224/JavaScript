9cfa671b4c03df94a549e52499284798e9312be5
austinwilson1224 <austin1224@gmail.com>
2021-03-19 15:29:23 -0700

    final changed

--- my-angular-site/src/app/core/core.module.ts
+++ my-angular-site/src/app/core/core.module.ts
@@ -1,12 +1,28 @@
+import { RouterModule } from '@angular/router';
+import { UserService } from './../user.service';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [
+    UserService
   ]
 })
 export class CoreModule { }
